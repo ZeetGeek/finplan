d95d1383ec15fba85d0dcef9eebd95ab0cc2098c
ZeetGeek <zeetgeek435@gmail.com>
2023-12-19 13:30:21 +0530

    footer completed

--- components/global/footer/index.js
+++ components/global/footer/index.js
@@ -1,22 +1,162 @@
 // Import dependencies here
+import Image from "next/image";
+import { Container, Row, Col } from "react-bootstrap";
+import Link from "next/link";
 
 // Define reusable components
+// import {} from "@/components/";
 
 // Include image assets if necessary
+import FooterLogo from "@/images/logo/logo.png";
 
 // Fetch or import data required for the page
+import { socialMedia, MenuLinks, TermsLink, addressLinks } from "@/data/footer";
 
 // Apply styles or link external stylesheets
-import style from "./footer.module.scss"
+import style from "./footer.module.scss";
 
 // Define the main functional component
 export const Footer = () => {
-     return (
-          <>
-               {/* Page content goes here */}
-               <h1>Footer</h1>
-          </>
-     );
+    return (
+        <>
+            {/* Page content goes here */}
+            <footer className={`${style.footer} footer-gl section-bg-color-1`} id="footer">
+                <Container>
+                    <Row>
+                        {/* footer column 1 */}
+                        <Col lg={5} className={`${style.column1}`}>
+                            <div className={`${style.column1Wrapper}`}>
+                                {/* footer logo */}
+                                <div className={`${style.logoWrapper}`}>
+                                    <Image
+                                        src={FooterLogo}
+                                        height={35}
+                                        width={150}
+                                        alt="finplan logo"
+                                    />
+                                </div>
+
+                                {/* footer description */}
+                                <div className={`${style.descriptionWrapper} mt-3`}>
+                                    <p className="text-medium gray-normal">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                                        elit tellus, luctus nec ullamcorper mattis, pulvinar.
+                                    </p>
+                                </div>
+
+                                {/* footer social links */}
+                                <div className={`${style.socialLinksWrapper}`}>
+                                    <ul className="d-flex align-items-center gap-3 mt-5">
+                                        {socialMedia.map((item) => {
+                                            return (
+                                                <li key={item.id}>
+                                                    <Link href={item.navigate} title={item.title}>
+                                                        {item.icon}
+                                                    </Link>
+                                                </li>
+                                            );
+                                        })}
+                                    </ul>
+                                </div>
+                            </div>
+                        </Col>
+
+                        {/* footer column 2 */}
+
+                        <Col lg={2} className={`${style.column2} ps-4`}>
+                            <div className={`${style.column2Wrapper}`}>
+                                {/* Links Title */}
+                                <h5 className=" mb-4 heading-h5 black-color weight-semibold">
+                                    Menu
+                                </h5>
+
+                                {/* All Links */}
+                                <ul>
+                                    {MenuLinks.map((item) => {
+                                        return (
+                                            <li key={item.id} className="mb-4">
+                                                <Link
+                                                    href={item.navigate}
+                                                    title={item.text}
+                                                    className="text-medium gray-normal"
+                                                >
+                                                    {item.text}
+                                                </Link>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            </div>
+                        </Col>
+
+                        {/* footer column 3 */}
+
+                        <Col lg={2} className={`${style.column3} ps-4`}>
+                            <div className={`${style.column3Wrapper}`}>
+                                {/* Links Title */}
+                                <h5 className=" mb-4 heading-h5 black-color weight-semibold">
+                                    Links
+                                </h5>
+
+                                {/* All Links */}
+                                <ul>
+                                    {TermsLink.map((item) => {
+                                        return (
+                                            <li key={item.id} className="mb-4">
+                                                <Link
+                                                    href={item.navigate}
+                                                    title={item.text}
+                                                    className="text-medium gray-normal"
+                                                >
+                                                    {item.text}
+                                                </Link>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            </div>
+                        </Col>
+
+                        {/* footer column 4 */}
+                        <Col lg={3} className={`${style.column4} ps-5`}>
+                            <div className={`${style.column4Wrapper}`}>
+                                {/* Links Title */}
+                                <h5 className=" mb-4 heading-h5 black-color weight-semibold">
+                                    Our Address
+                                </h5>
+
+                                {/* All Links */}
+                                <ul>
+                                    {addressLinks.map((item) => {
+                                        return (
+                                            <li key={item.id} className="mb-4">
+                                                <Link
+                                                    href={item.navigate}
+                                                    title={item.text}
+                                                    className="text-medium gray-normal"
+                                                >
+                                                    {item.text}
+                                                </Link>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            </div>
+                        </Col>
+
+                        {/* Copy right */}
+                        <Col lg={12}>
+                            <div className={`${style.copyRightWrapper}`}>
+                                <span className="text-medium text-center d-block gray-normal">
+                                    © 2023 Template Kit by Hello Word.
+                                </span>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </footer>
+        </>
+    );
 };
 
 // Export the component as default
---
+++ data/footer/index.js
@@ -0,0 +1,98 @@
+// footer social media icons
+import { Youtube, Facebook, Twitter, Linkedin } from "iconoir-react";
+
+// footer data
+
+// footer column 1
+
+export const socialMedia = [
+    {
+        id: 0,
+        icon: <Youtube />,
+        navigate: "https://www.youtube.com/",
+        title: "youtube",
+    },
+    {
+        id: 1,
+        icon: <Facebook />,
+        navigate: "https://www.facebook.com/",
+        title: "facebook",
+    },
+    {
+        id: 2,
+        icon: <Twitter />,
+        navigate: "https://twitter.com/",
+        title: "twitter",
+    },
+    {
+        id: 3,
+        icon: <Linkedin />,
+        navigate: "https://in.linkedin.com/",
+        title: "linkedin",
+    },
+];
+
+// footer column 2
+
+export const MenuLinks = [
+    {
+        id: 0,
+        text: "Home",
+        navigate: "/",
+    },
+    {
+        id: 0,
+        text: "About",
+        navigate: "/",
+    },
+    {
+        id: 0,
+        text: "Services",
+        navigate: "/",
+    },
+    {
+        id: 0,
+        text: "Contact",
+        navigate: "/",
+    },
+];
+
+// footer column 3
+
+export const TermsLink = [
+    {
+        id: 0,
+        text: "Terms of use",
+        navigate: "/",
+    },
+    {
+        id: 0,
+        text: "Privacy policy",
+        navigate: "/",
+    },
+    {
+        id: 0,
+        text: "Cookie policy",
+        navigate: "/",
+    },
+    {
+        id: 0,
+        text: "Terms & conditions",
+        navigate: "/",
+    },
+];
+
+// footer column 4
+
+export const addressLinks = [
+    {
+        id: 0,
+        text: "Jl. Niti Mandala Renon No.88, Denpasar, Bali - 80239",
+        navigate: "/",
+    },
+    {
+        id: 1,
+        text: "(316) 212-3456",
+        navigate: "/",
+    },
+];
